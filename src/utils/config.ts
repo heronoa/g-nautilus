1ad243434097bbffa10bc74e747cd6cd8ee8a969
Heron Amaral <heron.amaral@gmail.com>
2025-04-20 14:11:20 -0300

    refactor: refactor GitHub service configuration and tests

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,9 +1,9 @@
 interface config {
-  GITHUB_API_URL: string;
-  GITHUB_API_TOKEN: string;
+  githubApiUrl: string;
+  githubApiToken: string;
 }
 
 export const C: config = {
-  GITHUB_API_URL: "https://api.github.com",
-  GITHUB_API_TOKEN: process.env.GITHUB_API_TOKEN as string,
+  githubApiUrl: "https://api.github.com",
+  githubApiToken: process.env.GITHUB_API_TOKEN as string,
 };
